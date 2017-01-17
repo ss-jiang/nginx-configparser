280c32b287dbdae4e4e4e9270b8f2bc4e6f8d1b5
stan <stansjiang@gmail.com>
2017-01-17 01:26:10 -0800

    Created unit tests for nginx-configparser

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,6 +1,16 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
+class NSCP_Fixture_Test : public ::testing::Test {
+protected:
+ 	bool ParseString(const std::string config_string) {
+ 		std::stringstream config_stream(config_string);
+ 		return parser_.Parse(&config_stream, &out_config_);
+ 	}
+ 	NginxConfigParser parser_;
+ 	NginxConfig out_config_;
+ };
+
 TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfigParser parser;
   NginxConfig out_config;
@@ -8,4 +18,17 @@ TEST(NginxConfigParserTest, SimpleConfig) {
   bool success = parser.Parse("example_config", &out_config);
 
   EXPECT_TRUE(success);
+}
+
+TEST_F(NSCP_Fixture_Test, MissingEndBracket) {
+	std::string test_string = "server { listen   80; ";
+	EXPECT_FALSE(ParseString(test_string));
+}
+
+TEST_F(NSCP_Fixture_Test, MissingEndQuote) {
+	EXPECT_FALSE(ParseString("foo \"bar;"));
+}
+
+TEST_F(NSCP_Fixture_Test, EmptyStatement) {
+	EXPECT_TRUE(ParseString(";"));
 }
